e0dc21a3e976f2a290b44bc0a6b9ddb67440b1ef
aakash17kc <32451286+aakash17kc@users.noreply.github.com>
2018-04-22 14:17:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/article-one', function (req, res) {
+  res.send("Articel one");
 });
-app.get('/arcticel two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/arcticel-two', function (req, res) {
+  res.send("Articel TWO");
 });
-app.get('/articel three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/articel-three', function (req, res) {
+  res.send("ArticLE three");
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
