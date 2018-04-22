281180e2924593c243d0a29de2b01fe50c0aea94
aakash17kc <32451286+aakash17kc@users.noreply.github.com>
2018-04-22 14:26:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("Articel one");
+  res.sendFile(path.join(__dirname, 'ui', 'article.html'));
 });
 app.get('/arcticel-two', function (req, res) {
   res.send("Articel TWO");
