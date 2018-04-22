60dcc41201735df1f1bdfe4809c555f124efbbdd
aakash17kc <32451286+aakash17kc@users.noreply.github.com>
2018-04-22 14:50:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,44 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var content = {
+    title:" aakash",
+    heading:  "articel-one",
+    content:`<q> Examination</q>
+<blockquote> This is the definition</blockquote>
+<p> This  is a <ins>keyword</ins> done</p>
+<p> This  is a <del>keyword</del> done</p>
+<p> This is An <kbd>enter</kbd> key</p>
+<code>#include"stdio.h"</code>
+<p> This is <cite>Important</cite></p>`};
 
+
+function create_temp(data){
+    var content = data.content;
+var template = `<html>
+<body>
+<h1><b>CHAPTER</b> 1</h1> <!--This is th starting-->
+
+<p> Hello everyone </p>
+<hr> <!--for line-->
+<h2> <u>CHAPTER 1.1</u> </h2>
+<p><small> This    is     word
+   helping deck</small> </p>
+<p> Getting     to <br> know          everyone</p>
+<p><u> H<sub>2</sub>O</u></p>
+
+<strong> HEY THERE</strong>
+<em><mark> STUDY TODAY</mark> </em>
+<p> <abbr title="I am the king">IATK I Will be using IATK all the time</abbr>I Will be IATK using all the time</p>
+
+${content}
+</body>
+</html>`
+return template;
+}
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(content);
+  
 });
 app.get('/arcticel-two', function (req, res) {
   res.send("Articel TWO");
